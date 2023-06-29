26d698ad3814428cf773144a49b0cbfd44d4d309
Sudha Yadav <97832213+sudhaaaaaydv@users.noreply.github.com>
2023-06-29 10:24:47 +0530

    Add files via upload

---
+++ Recipe King/view/aboutAs.php
@@ -0,0 +1,141 @@
+<!DOCTYPE html>
+<html>
+	<title>About Us | Sri Lankan Easy Cook...
+	</title>
+
+	    <link rel="icon" href="img/recipe.jpeg">
+
+<head>
+
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+	<link rel="stylesheet" href="stylesheets/commenstyle.css">
+	<link rel="icon" href="img/recipe.jpeg">
+	<link rel="stylesheet" type="text/css" href="stylesheets/aboutAs.css">
+
+</head>
+<body>
+<!-- BACKGROUND IMAGE -->
+
+	<div class="bg-image"><img src="img/bg.jpg" width="100%" height="184%" ></div>
+
+	
+	<header>
+
+<!-- LOGO & HEADER -->
+
+		<div id="container">
+		   <div id="logo">
+					<img src = "img/recipe.jpeg", width = 400px, heigt = 180px, border = "4">
+		   </div> 
+
+		   <div id="name">
+
+			<h1 class="headding1" >Lankan Easy Cook...</h1>
+			<p class="headding2">Sri Lankan Largest Online Recipes</p>		
+			 </div>
+			 
+<!-- SEARCH BAR -->
+
+		   <div id="search-bar">
+            		<form>
+               	 		<input class="form-search" type="search" placeholder="Find a Recipe" aria-label="find">
+               			<button class="seach-button" type="submit" style="padding : 0px; border-radius: 0px;">Search</button>
+            		</form>
+
+        </div>
+
+<!-- LOGIN & REGISTER BUTTON -->
+
+
+			<div id="sign-button">
+				<a href="login.html"><button class="button-log">Login</button></a>
+				<a href="register.html"><button class="button-log">Register</button></a>
+   			</div>
+		</div>
+
+	</header>
+
+	<!-- NAV BAR -->
+
+	<hr class="navUp_hr">
+
+	<div class="nav">
+        <a button class="button" href="Lankan Easy Cook HOME.html" >Home</button></a>
+        <a button class="button" href="recipe.html" >Recipes</button></a>
+        <a button class="button" href="free.html">Free</button></a>
+         <a button class="button" href="contactUs.html">Contact Us</button></a>
+         <a button class="button" href="aboutAs.html" id="active">About Us</button></a>
+         <a button class="button" href="mycart.html">My Cart</button></a> 
+	</div>
+
+		<hr class="nav-hr">
+
+<!-- VIRTICAL LINE -->
+
+<div class="vl"></div>
+<br/>
+
+<!-- SIDE BAR -->
+
+
+<div class="side-bar">
+        <hr class="cat_hr">
+
+    <a button class="drop_bt" id="drop_bt1" href="Meal Type.html">&#9673; Meal Type</button></a>
+    <a button class="drop_bt" id="drop_bt2" href="Seasonal.html">&#9673; Seasonal </button></a>
+    <a button class="drop_bt" id="drop_bt3" href="Diet & Health.html">&#9673; Diet & Healthy</button></a>
+    <a button class="drop_bt" id="drop_bt4" href="Dish Type.html">&#9673; Dish Type</button></a>
+    <a button class="drop_bt" id="drop_bt5" href="World Cuisine.html">&#9673; World Cuisine</button></a>
+
+    <hr class="cat_hr-down">
+</div>
+
+
+<!-- FOOTER -->
+
+<div class="categories_box">Categories</div>
+
+
+<div class="fotter_logo" >
+		<img class="fotter_logo1" src="img/logo.png" >
+</div>
+
+<div class="fotter_logo" >
+		<img class="fotter_logo2" src="img/logo2.png" >
+</div>
+
+<hr id="fotter-hr">
+<p class = "footer_txt1">Copyright Statement</p>
+<p class = "footer_txt2">Privary Policy | Terms and Conditions</p>
+<hr class="fotter-hr">
+
+<!-- END COMMEN PARTS --> 
+
+<div class="writing-area"></div>
+
+<div class="writing-area-text">
+    <div class="header-name">
+            <h1>
+                About Us
+                <hr class="name-hr">
+            </h1>
+        </div>
+        
+        <div class="paragraph1">
+                <p><b>Lanka Easy cook</b>-Sri Lanka's latest and largest recipeies and offers.</p>
+            </div>
+        
+        <div class="missionvision">
+                <h1 class="name-hr2">VISION</h1>
+                <p>At Vision Personal Training, we believe that nutrition is the key to your success in achieving your health goals. </p>
+                <br><br>
+                <h1 class="name-hr2">MISSION</h1>
+                <p>Our Mission is to provide fresh, healthy, nutritious and great tasting food at reasonable prices in a clean, friendly and<br/><br/> convenient environment....</p>
+                
+                
+            </div>
+ </div>>
+
+</body>
+</html>
